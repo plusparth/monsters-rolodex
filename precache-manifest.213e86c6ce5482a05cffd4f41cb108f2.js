c1cb07d404f3031a0707230c0686ef67c5f62b4a
Parth Oza <plusparth@gmail.com>
2020-06-22 10:19:26 -0400

    Updates

---
+++ precache-manifest.213e86c6ce5482a05cffd4f41cb108f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821e86f5d07c9e91d16838a299d8fa83",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b53fba0eee9412998202",
+    "url": "/monsters-rolodex/static/css/main.bc0135f2.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b53fba0eee9412998202",
+    "url": "/monsters-rolodex/static/js/main.8e3e7c72.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
